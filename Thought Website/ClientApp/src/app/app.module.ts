388bd777a466808ce484352609747ccd3ccfb008
Sang Mai <sangmai913@gmail.com>
2020-08-24 00:02:31 -0500

    Initial Commit

--- Thought Website/ClientApp/src/app/app.module.ts
+++ Thought Website/ClientApp/src/app/app.module.ts
@@ -1,22 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { PhilosophyComponent } from './philosophy/philosophy.component';
+import { VideosComponent } from './videos/videos.component';
+import { LogsComponent } from './logs/logs.component';
+import { ResourcesComponent } from './resources/resources.component';
+import { GuidesComponent } from './guides/guides.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent,
-    FetchDataComponent
+    PhilosophyComponent,
+    VideosComponent,
+    LogsComponent,
+    ResourcesComponent,
+    GuidesComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -24,8 +30,11 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
+      { path: 'philosophy', component: PhilosophyComponent },
+      { path: 'videos', component: VideosComponent },
+      { path: 'logs', component: LogsComponent },
+      { path: 'resources', component: ResourcesComponent },
+      { path: 'guides', component: GuidesComponent },
     ])
   ],
   providers: [],
