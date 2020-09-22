72c086b38b138092e28d46b3e5bb50f70d59c2cf
Trystan Alexander <alexandt96@gmail.com>
2020-09-22 01:06:24 -0500

    tried some stuff - still sad

--- Thought Website/ClientApp/src/app/roster/roster.component.ts
+++ Thought Website/ClientApp/src/app/roster/roster.component.ts
@@ -22,19 +22,26 @@ export class RosterComponent implements OnInit {
   constructor(private readonly blizzApiService: BlizzApiService, private readonly raiderIoService: RaiderIoService) {}
 
   ngOnInit() {
-    this.blizzApiService.getAccessToken().subscribe(
-      accessToken => {
-        this.accessToken = accessToken;
-        this.getOfficerRoster();
-        this.getLeaderRoster();
-        this.getMythicCoreRoster();
-        this.isLoaded = true;
-      });
+    this.getOfficerRoster();
+    this.getLeaderRoster();
+    this.getMythicCoreRoster();
+    this.isLoaded = true;
   }
+  // getOfficerRoster(){
+  //   this.blizzApiService.getGuildRoster().then(res => {
+  //     const guild = res.data
+  //     const officers = guild.members.filter( member => member.rank === 2 || member.rank === 3)
+  //     forkJoin(
+  //       officers.map(member => {
+  //         return this.raiderIoService.getCharacterInformation(member.character.name);
+  //       })
+  //     ).subscribe(allResults => { this.officerRoster = allResults; this.getCharacterRendersOfficers() });
+  //   })
+  // }
 
   // Get guild roster and filter down to only officers/raid leads.
   getOfficerRoster() {
-    this.blizzApiService.getGuildRoster(this.accessToken.access_token).subscribe(
+    this.blizzApiService.getGuildRoster().subscribe(
       guild => {
         var officers = guild.members.filter(element => element.rank === 2 || element.rank === 3);
         forkJoin(
@@ -53,10 +60,10 @@ export class RosterComponent implements OnInit {
       })
     ).subscribe(allResults => { this.guildMasterRoster = allResults; this.getCharacterRendersGuildLeaders();});
   }
-
-  // Get guild roster and filter down to only mythic core roster.
+  //
+  // // Get guild roster and filter down to only mythic core roster.
   getMythicCoreRoster() {
-    this.blizzApiService.getGuildRoster(this.accessToken.access_token).subscribe(
+    this.blizzApiService.getGuildRoster().subscribe(
       guild => {
         var mythicCore = guild.members.filter(element => element.rank === 5 || element.rank === 2 || element.rank === 3);
         forkJoin(
@@ -66,26 +73,25 @@ export class RosterComponent implements OnInit {
         ).subscribe(allResults => { this.mythicCoreRoster = allResults; this.getCharacterRendersMythicCore(); });
       });
   }
-
-  // Get character renders for mythic core team, store in appropriate character objects.
+  // // Get character renders for mythic core team, store in appropriate character objects.
   getCharacterRendersMythicCore() {
     this.mythicCoreRoster.forEach(member => {
-      this.blizzApiService.getCharacterRender(this.accessToken.access_token, member.name.toLowerCase()).subscribe(
+      this.blizzApiService.getCharacterRender(member.name.toLowerCase()).subscribe(
         result => { member.render_url = result.render_url });
     });
   }
   // Get character renders for officers, store in appropriate character objects.
   getCharacterRendersOfficers() {
     this.officerRoster.forEach(member => {
-      this.blizzApiService.getCharacterRender(this.accessToken.access_token, member.name.toLowerCase()).subscribe(
+      this.blizzApiService.getCharacterRender(member.name.toLowerCase()).subscribe(
         result => { member.render_url = result.render_url });
     });
   }
-
-  // Get character renders for guild leaders, store in appropriate character objects.
+  //
+  // // Get character renders for guild leaders, store in appropriate character objects.
   getCharacterRendersGuildLeaders() {
     this.guildMasterRoster.forEach(member => {
-      this.blizzApiService.getCharacterRender(this.accessToken.access_token, member.name.toLowerCase()).subscribe(
+      this.blizzApiService.getCharacterRender(member.name.toLowerCase()).subscribe(
         result => { member.render_url = result.render_url });
     });
   }
--- Thought Website/ClientApp/src/services/blizz-api.service.ts
+++ Thought Website/ClientApp/src/services/blizz-api.service.ts
@@ -5,36 +5,27 @@ import { IGuild } from "../app/interfaces/IGuild";
 import { map, catchError } from "rxjs/operators";
 import { Observable } from "rxjs/internal/Observable";
 import { ICharacterRender } from "../app/interfaces/ICharacterRender";
+import * as firebase from "firebase/app";
+import axios from 'axios'
+
 
 @Injectable({
   providedIn: "root"
 })
 export class BlizzApiService {
   constructor(private http: HttpClient) { }
-  blizzHostName = "https://us.api.blizzard.com";
-  // Get access token using blizz client id and secret
-  getAccessToken() {
-   const httpHeaders = new HttpHeaders()
-     .set("Authorization", `Basic ` + btoa(`${environment.clientId}:${environment.clientSecret}`))
-     .set("Content-Type", "application/x-www-form-urlencoded")
-     .set("Accept", "*/*");
-    var body = new HttpParams().set("grant_type", "client_credentials")
-    return this.http.post("https://us.battle.net/oauth/token", body, { 'headers': httpHeaders });
-  }
 
-  getGuildRoster(accessToken): Observable<IGuild> {
+  getGuildRoster(): Observable<IGuild>{
     const headers = new HttpHeaders()
       .set("Content-Type", "application/json")
-      .set("Authorization", `Bearer ${accessToken}`)
       .set("Battlenet-Namespace", "profile-us");
-    return this.http.get<IGuild>(this.blizzHostName + "/data/wow/guild/sargeras/thought/roster", { 'headers': headers })
+    return this.http.get<IGuild>('https://us-central1-thought-website.cloudfunctions.net/getGuildRoster', { 'headers': headers })
   }
 
-  getCharacterRender(accessToken, charName): Observable<ICharacterRender> {
+  getCharacterRender(charName): Observable<ICharacterRender> {
     const headers = new HttpHeaders()
       .set("Content-Type", "application/json")
-      .set("Authorization", `Bearer ${accessToken}`)
       .set("Battlenet-Namespace", "profile-us");
-    return this.http.get<ICharacterRender>(this.blizzHostName + `/profile/wow/character/sargeras/${charName}/character-media`, {'headers' : headers })
+    return this.http.get<ICharacterRender>(`https://us-central1-thought-website.cloudfunctions.net/getCharacterRender?charName=${charName}`, {'headers' : headers })
   }
 }
